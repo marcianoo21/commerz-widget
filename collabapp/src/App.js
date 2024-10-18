d2b94eda1743d3dc0ce325cb56798524724e60f0
247034 Aleksander Moszyński <247034@edu.p.lodz.pl>
2024-10-18 17:21:46 +0200

    UpperTab + prototype of calendar

--- collabapp/src/App.js
+++ collabapp/src/App.js
@@ -1,11 +1,13 @@
 import './styles/App.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import CalendarModal from './components/MainCalendar.jsx'
+import UpperTab from './components/UpperTab.jsx'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
 	return (
 		<ChakraProvider>
-			<CalendarModal />
+			<UpperTab />
 		</ChakraProvider>
 	)
 }
